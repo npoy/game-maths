4611ee923b216ea88ac67a7c5fff746a2b3cfbdf
npoy <nicolas.poy@outlook.com>
2023-04-25 23:30:07 -0300

    Added attributes and big flag off

--- Assets/AttributeManager.cs
+++ Assets/AttributeManager.cs
@@ -2,10 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class AttributeManager : MonoBehaviour
 {
+    static public int MAGIC = 16;
+    static public int INTELLIGENCE = 8;
+    static public int CHARISMA = 4;
+    static public int FLY = 2;
+    static public int INVISIBLE = 1;
+    
     public Text attributeDisplay;
+    int attributes = 0;
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "MAGIC") {
+            attributes |= MAGIC;
+        } else if (other.gameObject.tag == "INTELLIGENCE") {
+            attributes |= INTELLIGENCE;
+        } else if (other.gameObject.tag == "CHARISMA") {
+            attributes |= CHARISMA;
+        } else if (other.gameObject.tag == "FLY") {
+            attributes |= FLY;
+        } else if (other.gameObject.tag == "INVISIBLE") {
+            attributes |= INVISIBLE;
+        } else if (other.gameObject.tag == "ANTIMAGIC") {
+            attributes &= ~MAGIC;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +42,6 @@ public class AttributeManager : MonoBehaviour
     {
         Vector3 screenPoint = Camera.main.WorldToScreenPoint(this.transform.position);
         attributeDisplay.transform.position = screenPoint + new Vector3(0,-50,0);
+        attributeDisplay.text = Convert.ToString(attributes, 2).PadLeft(8, '0');
     }
-       
 }
