4501e1fc35f0400d44f7ac5a229d120bf7da8282
Nicolas Poy <nicolas.poy@corp.smtsoftware.com>
2023-05-03 10:42:36 -0300

    Added bit toggling and unity specifics

--- Assets/Scripts/AttributeManager.cs
+++ Assets/Scripts/AttributeManager.cs
@@ -6,6 +6,12 @@ using System;
 
 public class AttributeManager : MonoBehaviour
 {
+    static public int MAGIC = 16;
+    static public int INTELLIGENCE = 8;
+    static public int CHARISMA = 4;
+    static public int FLY = 2;
+    static public int INVISIBLE = 1;
+
     static public int BLUEKEY = 4;
     static public int REDKEY = 2;
     static public int GREENKEY = 1;
@@ -14,6 +20,28 @@ public class AttributeManager : MonoBehaviour
     public int attributes = 0;
 
     private void OnTriggerEnter(Collider other) {
+        // First group of doors
+        if (other.gameObject.tag == "MAGIC") {
+            attributes ^= MAGIC;
+        } else if (other.gameObject.tag == "INTELLIGENCE") {
+            attributes |= INTELLIGENCE;
+        } else if (other.gameObject.tag == "CHARISMA") {
+            attributes |= CHARISMA;
+        } else if (other.gameObject.tag == "FLY") {
+            attributes |= FLY;
+        } else if (other.gameObject.tag == "INVISIBLE") {
+            attributes |= INVISIBLE;
+        } else if (other.gameObject.tag == "ANTIMAGIC") {
+            attributes &= ~MAGIC;
+        } else if (other.gameObject.tag == "REMOVE") {
+            attributes &= ~(INTELLIGENCE | MAGIC);
+        } else if (other.gameObject.tag == "ADD") {
+            attributes |= (INTELLIGENCE | MAGIC | CHARISMA);
+        } else if (other.gameObject.tag == "RESET") {
+            attributes = 0;
+        }
+
+        // Keys and doors
         if (other.gameObject.tag == "BLUE_KEY") {
             attributes |= BLUEKEY;
             Destroy(other.gameObject);
